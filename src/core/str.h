27af8045b32e4c2b98c7e78db07a924a6ac50c53
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2014-02-06 01:30:18 +0800

    Added static "String::from<T>(...)" generator (with some tests).

--- src/core/str.h
+++ src/core/str.h
@@ -109,9 +109,20 @@ namespace Try
         bool operator != (const String& str) const;
 
         static String join(const StringList& parts, const String& sep);
+
+        template<typename T>
+        static String from(const T& src);
     };
 }
 
 std::ostream& operator << (std::ostream& os, const Try::String& str);
+
+template<typename T>
+Try::String Try::String::from(const T& src)
+{
+    std::stringstream buff;
+    buff << src;
+    return buff.str();
+}
 
 #endif // TRY_STRING_H
---
+++ src/tests/string/main.cpp
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the Try! Engine project.
+ *
+ * Copyright 2010-2011 Emanuele Bertoldi. All rights reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * You should have received a copy of the modified BSD License along with this
+ * program. If not, see <http://www.opensource.org/licenses/bsd-license.php>
+ */
+
+#include <vector3.h>
+#include <str.h>
+#include "catch.hpp"
+
+TEST_CASE( "Test generating strings from different sources using 'from<T>(...)'", "[string][from]" )
+{
+    REQUIRE(Try::String::from<const char*>("Try!") == "Try!");
+    REQUIRE(Try::String::from<std::string>("Try!") == "Try!");
+    REQUIRE(Try::String::from<bool>(true) == "1");
+    REQUIRE(Try::String::from<int>(567) == "567");
+    REQUIRE(Try::String::from<float>(23.56) == "23.56");
+    REQUIRE(Try::String::from<Try::Vector3>(Try::Vector3(23.56, 12, 45)) == "Vector3(23.56, 12, 45)");
+}
